2615b59a9c36a5276b87fde8aba7f68fbf4487a5
Vanessa Quero <vanessatkd223@gmail.com>
2022-07-24 22:51:43 -0400

    travel app page with dark mode and mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,61 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/index.html","./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'bali': "url('../img/bali.jpg')",
+        'chicago': "url('../img/chicago.jpg')",
+        'yosemite': "url('../img/yosemite.jpg')",
+        'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+        'europe' : "url('../img/europe.jpg')",
+        'iceland': "url('../img/iceland.jpg')",
+        'LA': "url('../img/LA.jpg')",
+        'miami': "url('../img/miami.jpg')",
+        'new_york': "url('../img/new_york.jpg')",
+        'norway': "url('../img/norway.jpg')",
+        'seattle': "url('../img/seattle.jpg')",
+        'switzerland': "url('../img/switzerland.jpg')",
+        'sydney': "url('../img/sydney.jpg')"
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#7EBAE5',
+        'secondary': '#F9ECCD',
+        'terciary': '#FFFBF1',
+        'quartiary': '#FCA371',
+        'quintiary': '#A3440E',
+        'sixtiary': '#321302',
+        'septuary': '#CE4C03'
+      }),
+      textColor:{
+        'primary': '#7EBAE5',
+        'secondary': '#F9ECCD',
+        'terciary': '#FFFBF1',
+        'quartiary': '#FCA371',
+        'quintiary': '#A3440E',
+        'sixtiary': '#321302',
+        'septuary': '#CE4C03'
+      },
+      fontFamily:{
+        Glegoo:['Glegoo','sans-serif']
+      },
+      ringColor:{
+        'primary': '#7EBAE5',
+        'secondary': '#F9ECCD',
+        'terciary': '#FFFBF1',
+        'quartiary': '#FCA371',
+        'quintiary': '#A3440E',
+        'sixtiary': '#321302',
+        'septuary': '#CE4C03'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
